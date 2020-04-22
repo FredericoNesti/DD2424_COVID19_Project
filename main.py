be4ce6b460d8faa41af4784f577340eac9681a7c
Frederico Nesti <47975100+FredericoNesti@users.noreply.github.com>
2020-04-22 07:09:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,56 @@
 
-from Dataloader import *
-import argparse
+
 import time
+import argparse
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+from torchvision.transforms import ToTensor
+
+from Augmentate import *
 
 parser = argparse.ArgumentParser(description='COVID19')
-parser.add_argument('--dataset', type=str, default='', metavar='N', help='')
+
+# training params
 parser.add_argument('--device', type=str, default='cuda', metavar='N', help='')
 parser.add_argument('--batch', type=int, default='', metavar='N', help='')
-parser.add_argument('--splitset', nargs='+',type=int, default=[0.6,0.1,0.3], metavar='N',help='')
+parser.add_argument('--epochs', type=int, default=1, metavar='N',help='')
+
+# data sources
+parser.add_argument('--train_folder', type=str, default='', metavar='N',help='')
+parser.add_argument('--validation_folder', type=str, default='', metavar='N',help='')
+parser.add_argument('--test_folder', type=str, default='', metavar='N',help='')
+
+# results params
+parser.add_argument('--results_directory', type=str, default='', metavar='N',help='')
+
 args = parser.parse_args()
 
 if __name__ == "__main__":
     start_time = time.time()
 
-    COVID19_database = DATABASE(batch=args.batch,
-                                split=args.splitset,
-                                device=args.device)
+    train_dataset = ImageFolder(root=args.train_folder, transform=Augmentation())
+    train_loaded = DataLoader(train_dataset, batch_size=args.batch, shuffle=True)
 
-    train_set, valid_set, test_set = COVID19_database.split_database()
+    val_dataset = ImageFolder(root=args.validation_folder, transform=Augmentation())
+    val_loaded = DataLoader(val_dataset, batch_size=args.batch, shuffle=True)
+
+    # test dataset with augmentation
+    test_dataset_A = ImageFolder(root=args.test_folder, transform=Augmentation())
+    test_loaded_A = DataLoader(test_dataset_A, batch_size=args.batch, shuffle=True)
+
+    # test dataset without augmentation
+    test_dataset_N = ImageFolder(root=args.test_folder, transform=ToTensor())
+    test_loaded_N = DataLoader(test_dataset_N, batch_size=args.batch, shuffle=True)
+
+    for e in range(1, args.epochs+1):
+        print('Epoch: ', e)
+        # training comes here
+
+
 
     elapsed_time = time.time() - start_time
     time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
+
+
 
 
