a1edf4e9609431d80fea096e75367b15af774954
matheusoliveirafranca <matheusfranca.t19@gmail.com>
2020-04-29 17:53:14 +0200

    debug

--- run_model.py
+++ run_model.py
@@ -84,4 +84,6 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), momentum=0.1, lr=0.05, weight_decay=0.002)
 scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=10e-5, max_lr=0.1)
 
-acc_tr_bn, acc_test_bn, loss_tr_bn = train_my_model(net, criterion, optimizer, scheduler, verbose)
+print(torch.cuda.get_device_properties(device).total_memory)
+
+# acc_tr_bn, acc_test_bn, loss_tr_bn = train_my_model(net, criterion, optimizer, scheduler, verbose)
